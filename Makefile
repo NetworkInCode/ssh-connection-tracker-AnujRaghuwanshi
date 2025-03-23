1c8da515e317fd6766f75526e6a4af2dd6174c76
Anuj <anujraghuwanshipip04@gmail.com>
2025-03-23 21:01:11 +0530

    Implementated repo

--- Makefile
+++ Makefile
@@ -1,1 +1,19 @@
+BPF_OBJ = bpf_prog.bpf.o
+USER_BIN = ssh_audit
 
+CC = clang
+BPFTOOL = bpftool
+LIBBPF_DIR = /usr/lib/libbpf
+CFLAGS = -I/usr/include -Wall -O2
+LDFLAGS = -lelf -lz -lbpf
+
+all: $(BPF_OBJ) $(USER_BIN)
+
+$(BPF_OBJ): bpf_prog.c
+	clang -target bpf -D__KERNEL__ -Wall -O2 -c $< -o $@
+
+$(USER_BIN): main.c $(BPF_OBJ)
+	$(CC) $(CFLAGS) -o $@ $< $(LDFLAGS)
+
+clean:
+	rm -f $(BPF_OBJ) $(USER_BIN)
--- src/main.c
+++ src/main.c
@@ -1,1 +1,43 @@
+/* main.c - User-space program to load eBPF and interact with it */
+#include <stdio.h>
+#include <stdlib.h>
+#include <bpf/libbpf.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include "bpf_prog.skel.h"
+
+static volatile int keep_running = 1;
+
+void sigint_handler(int sig) {
+    keep_running = 0;
+}
+
+int main() {
+    struct bpf_prog *skel;
+    int err;
+
+    signal(SIGINT, sigint_handler);
+
+    skel = bpf_prog__open_and_load();
+    if (!skel) {
+        fprintf(stderr, "Failed to load eBPF program\n");
+        return 1;
+    }
+
+    err = bpf_prog__attach(skel);
+    if (err) {
+        fprintf(stderr, "Failed to attach eBPF program\n");
+        goto cleanup;
+    }
+
+    printf("Tracking SSH connections... Press Ctrl+C to exit.\n");
+    while (keep_running) {
+        sleep(1);
+    }
+
+cleanup:
+    bpf_prog__destroy(skel);
+    return 0;
+}
 
